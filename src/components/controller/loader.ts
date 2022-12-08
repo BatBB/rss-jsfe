352eeccf12f22bc2c4c07a58062d4e32da0cd2a4
Bator B <batorbb@gmail.com>
2022-12-09 00:56:52 +0100

    feat: migrate loader.js

--- src/components/controller/loader.ts
+++ src/components/controller/loader.ts
@@ -1,11 +1,12 @@
+
 class Loader {
-  constructor(baseLink, options) {
-    this.baseLink = baseLink;
-    this.options = options;
-  }
+  constructor(
+    private baseLink: string,
+    private options: { sources?: string },
+  ) {}
 
   getResp(
-    { endpoint, options = {} },
+    { endpoint, options = {} }: { endpoint: string, options: { sources?: string}},
     callback = () => {
       console.error("No callback for GET response");
     }
@@ -13,7 +14,7 @@ class Loader {
     this.load("GET", endpoint, callback, options);
   }
 
-  errorHandler(res) {
+  errorHandler(res: Response) {
     if (!res.ok) {
       if (res.status === 401 || res.status === 404)
         console.log(
@@ -25,8 +26,8 @@ class Loader {
     return res;
   }
 
-  makeUrl(options, endpoint) {
-    const urlOptions = { ...this.options, ...options };
+  makeUrl(options: { sources?: string }, endpoint: string) {
+    const urlOptions: { [key: string]: string} = { ...this.options, ...options };
     let url = `${this.baseLink}${endpoint}?`;
 
     Object.keys(urlOptions).forEach((key) => {
@@ -36,7 +37,12 @@ class Loader {
     return url.slice(0, -1);
   }
 
-  load(method, endpoint, callback, options = {}) {
+  load(
+    method: string,
+    endpoint: string,
+    callback: (data: string) => void,
+    options: { sources?: string } = {}
+  ) {
     fetch(this.makeUrl(options, endpoint), { method })
       .then(this.errorHandler)
       .then((res) => res.json())
