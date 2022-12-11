a5724fa22a16d83befbb64f23bfd698e8a00385c
Bator B <batorbb@gmail.com>
2022-12-11 19:03:42 +0100

    fix: change URL for deploy

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from "./loader";
 
 class AppLoader extends Loader {
   constructor() {
-    super("https://newsapi.org/v2/", {
+    super("https://nodenews.up.railway.app/", {   //https://newsapi.org/v2/
       apiKey: "ce877a3f201441f092f3e0f41edc87e4", // получите свой ключ https://newsapi.org/
     });
   }
